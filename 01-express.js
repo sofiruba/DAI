8a344defb7e3c49cb63e74a7a03c9bf6a78b2bb4
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-05-12 08:08:49 -0300

    Update 01-express.js

--- 01-express.js
+++ 01-express.js
@@ -1,41 +1,121 @@
 const express = require('express');
-const req = require('express/lib/request');
 const app = express();
 const port = 3000;
- 
-const process_data = () => {
-   let x = 1;
-   return{
-       resultado: x
-   }
-}
- 
 app.use(express.json());
- 
-app.post('/', (req, res) => {
- 
-    console.log(req.body);
-    var cartones = [];
-    let random  = () => { 
-        return  Math.floor(Math.random() * (max - min + 1) + min);
+
+    function NumeroAleatorio(max){
+        return Math.floor(Math.random() * (max - 1)) + 1;
     }
-    max = [...req.body];
-    for(let i = 0; i<max; i++){
-        cartones[i] ={
-            numeros:[],
-            numero : random(),
+
+    let cartones = [];
+
+    function crearCarton(){
+        let carton = {
+            nombre: null,
+            valores: [],      
         }
-        cartones[i].numeros[i].push(random());
+        for (let index = 0; index < 10; index++) {
+            carton.valores.push(NumeroAleatorio(10))
+        
+        }
+        return carton;
     }
-    res.send([cartones]);
 
-});
+    function crearCartones(num){
 
-app.get("/mi_endpoint", (req,res)=>{
-    res.send("respuesta");
-})
- 
-app.listen (port, () => {
-   
-    console.log(`servidor en port ${port}`)
-})
+        for (let index = 0; index < num; index++) {
+            cartones.push(crearCarton(10))
+        
+        }
+        return cartones;
+    }
+
+
+    function iniciarJuego(num){
+        return crearCartones(num);
+    }
+
+    let nombres = [];
+    function obtenerCarton(nombre){
+        nombres.push(nombre);
+        for (let index = 0; index < cartones.length; index++) {
+            if (cartones[index].carton.nombre = nombres[index]){
+                return cartones[index].carton.valores;
+            } 
+            else{
+                return -1; 
+            }
+            
+        }
+
+    }
+
+    function devolverCartones(nombre){
+        if(nombre == ""){
+            return cartones;
+        }
+        else{
+            return obtenerCarton(nombre);
+        }
+    }
+    let numerosSacados = [];
+
+    function sacarNumero(num){
+        let numeroRandom = NumeroAleatorio(num);
+        numerosSacados.push(numeroRandom);
+        cartones.forEach(c => {
+             for (let i = 0; i > c.valores; i++) {
+                 if (c.carton.valores[i] = numerosSacados[i]){
+                     c.carton.valores[i].pop;
+                     return numerosSacados;
+                 }
+                 else {
+                     return -1;
+                 }
+             } 
+         })
+    }
+
+       
+
+    app.post('/numero_aleatorio', (req, res)=>{
+    
+        console.log(req.body.numero);
+        let x = NumeroAleatorio(req.body.numero);
+        res.send([x]);
+        
+        })
+        
+        
+        
+    app.post('/iniciar_Juego', (req, res)=>{
+        
+        console.log(req.body.cartones);    
+        let y = iniciarJuego(req.body.cartones);    
+        res.send([y]);
+        
+        
+    })
+
+    app.get('/obtener_Carton', (req, res)=>{
+        console.log(req.body.nombre); 
+         let s = obtenerCarton(req.body.nombre);    
+         res.send(s);
+       
+     
+    })
+
+    app.get('/cartones', (req, res)=>{
+        console.log(req.body.nombre);
+        let cartonesAMostrar = devolverCartones(req.body.nombre);
+        res.send(cartonesAMostrar);
+    })
+
+    app.get('/sacar_numero', (req, res)=> {
+        console.log(req.body.numero);
+        let sacarNumeros = sacarNumero(req.body.numero);
+        res.send(sacarNumeros);
+    })
+    app.listen(port, () => {
+        console.log(`example app listening on port ${port}`);
+    });
