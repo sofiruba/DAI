0a6c684bd3ae0ac3245dfcce74cf05e94e90df7b
meliiS13 <102042430+meliiS13@users.noreply.github.com>
2022-08-22 12:12:04 -0300

    detalle, login

--- tp5/src/screens/login.js
+++ tp5/src/screens/login.js
@@ -1,5 +1,5 @@
 import React , {useState} from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 import axios from 'axios'
 import {useNavigation} from '@react-navigation/native';
 
@@ -20,9 +20,14 @@ export default function Login() {
     }
     return (
         <View style={styles.container}>
+            <View>
+                <Text style={styles.titulo}>RESTAURANTE</Text>
+            </View>
             <TextInput style={styles.input} onChangeText={(text) => setEmail(text)} placeholder="Email"></TextInput>
             <TextInput secureTextEntry={true} style={styles.input} onChangeText={(text) => setPassword(text)} placeholder="Password"></TextInput>
-            <Button  title="Login" onPress={()=> login({"email": email, "password": password})} />
+            <View style={styles.boton} >
+            <Button  title="Login" color="#88ba93" onPress={()=> login({"email": email, "password": password})} />
+            </View>
         </View>
     )
 } 
@@ -30,18 +35,27 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
         alignItems: 'center',
-        padding: '11%',
-        marginTop: '10%',
-        backgroundColor: 'pink',
+        padding: 10,
+        marginTop: 3,
+        backgroundColor: '#deffe5',
         height: '100%',
         width: '100%',
       },
       input: {
         backgroundColor: 'white',
-        borderRadius: 20,
-        marginTop: '5%',
+        borderRadius: 10,
+        marginTop: 20,
         width: 200,
         height: 45,
         marginLeft: '2%',
+      },
+      boton: {
+        marginTop: 20,
+        borderRadius: 20,
+      },
+      titulo: {
+        fontSize: 30,
+        marginBottom: 50,
+        borderRadius: 20,
       },
 })
