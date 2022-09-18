99aa0ede54659c1c0f67e0591ffda299f59021df
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-09-18 00:34:20 -0300

    Update App.js

--- tp5/App.js
+++ tp5/App.js
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 import Main from './src/navigation/main';
 export const PlatosContext = createContext([])
 export const UserContext = createContext({})
+export const MenuContext = createContext([])
 export default function App() {
 
   return (
