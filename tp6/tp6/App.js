990851ae8fda78aecd9b1d8fea525bd66ae02aad
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-10-06 10:09:47 -0300

    Lol easy

---
+++ tp6/tp6/App.js
@@ -0,0 +1,115 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Alert, Modal, Button, Pressable } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const crearAlerta = () =>
+    Alert.alert(
+      "Esto es una alerta",
+      "Aca va el mensaje",
+      [
+        {
+          text: "Cerrar",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+
+  return (
+
+    <View style={styles.centeredView}>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={crearAlerta}
+      >
+        <Text style={styles.textStyle}>Ver Alert</Text>
+      </Pressable>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal cerrado");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Esto es un modal</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Cerrar Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Ver Modal</Text>
+      </Pressable>
+      <StatusBar style="auto" />
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
