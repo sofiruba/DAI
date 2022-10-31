018b5451510d35298427f4efffd55b5007f2c7b3
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-10-31 11:27:29 -0300

    cositas

---
+++ tp_final/screens/contactos.js
@@ -0,0 +1,3 @@
+/* Pantalla de visualización de todos los Contactos del Teléfono utilizando la FlatList. 
+Se debe mostrar el Nombre y Apellido / Número telefónico y un indicador si es el Numero de Contacto predeterminado de emergencia (con algún icono o similar) 
+*/
---
+++ tp_final/screens/horaytemperatura.js
@@ -0,0 +1,3 @@
+/* La aplicación debe contar con una pantalla de visualización de la fecha y hora, temperatura actual (API de clima / Axios) y ubicación.
+ La Temperatura se debe poder obtener según el geoposicionamiento del dispositivo (Location). 
+*/
