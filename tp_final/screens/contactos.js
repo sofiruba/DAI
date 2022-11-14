afe9fe148b12b5a1a0289bd810a998c18a66bf1c
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-11-14 18:02:31 -0300

    contactos y permisos, falta hacer la flatlist

--- tp_final/screens/contactos.js
+++ tp_final/screens/contactos.js
@@ -1,11 +1,95 @@
-/* Pantalla de visualización de todos los Contactos del Teléfono utilizando la FlatList. 
-Se debe mostrar el Nombre y Apellido / Número telefónico y un indicador si es el Numero de Contacto predeterminado de emergencia (con algún icono o similar) 
-*/
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { useEffect, useState } from 'react';
 
-import { View } from "react-native";
+export default function Contactos() {
+    let [error, setError] = useState(undefined);
+    let [contactos, setContactos] = useState(undefined);
+    const [search, setSearch] = useState('')
 
-export default function Contactos(){
-    return(
-        <View></View>
-    )
+    useEffect(() => {
+        (async () => {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === "granted") {
+                const { data } = await Contacts.getContactsAsync({
+                    fields: [Contacts.Fields.Birthday, Contacts.Fields.Emails, Contacts.Fields.FirstName, Contacts.Fields.LastName, Contacts.Fields.PhoneNumbers]
+                });
+
+                if (data.length > 0) {
+                    setContactos(data);
+                } else {
+                    setError("No se han encontrado contactos");
+                }
+            } else {
+                setError("El acceso ha sido denegado.");
+            }
+        })();
+    }, []);
+
+    let getContactData = (data, property) => {
+        if (data) {
+            return data.map((data, index) => {
+                return (
+                    <View key={index}>
+                        <Text>{data.label}: {data[property]}</Text>
+                    </View>
+                )
+            });
+        }
+    }
+
+    let getContactRows = () => {
+        if ((contactos !== undefined)) {
+
+            return contactos.map((contact, index) => {
+                return (
+                    <View key={index} style={styles.contact}>
+                       { /* <SafeAreaView>
+            <FlatList 
+            data = {contact}
+            extraData= {contact}
+            keyExtractor= {index => index.toString()}
+            renderItem = {  }
+            ItemSeparatorComponent = {() => <View style= {{ marginVertical: 10, borderColor: '#00000020', borderWidth: 1, marginHorizontal: 10,}}/>}
+            ListHeaderComponent = {()=><Text style = {{fontWeight: 'bold', marginBottom: 10, marginHorizontal: 10, fontSize: 15}}>Contactos</Text>}
+            />
+                </SafeAreaView> */}
+                        <Text>Name: {contact.firstName} {contact.lastName}</Text>
+                        {contact.birthday ? <Text>Birthday: {contact.birthday.month}/{contact.birthday.day}/{contact.birthday.year}</Text> : undefined}
+                        {getContactData(contact.phoneNumbers, "number")}
+                        {getContactData(contact.emails, "email")}
+                    </View>
+                );
+            });
+
+        } else {
+            return <Text>Esperando Contactos...</Text>
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={{ marginTop: 80 }}>ESTA ES SU LISTA DE CONTACTOS:</Text>
+            <ScrollView>
+                {getContactRows()}
+            </ScrollView>
+            <Text>{error}</Text>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    contact: {
+        marginVertical: 8,
+        marginBotton: 50,
+        marginTop: 32
+    }
+});
