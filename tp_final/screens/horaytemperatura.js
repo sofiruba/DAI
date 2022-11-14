83b79441b189f7eafd0632971db5362707099deb
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-11-14 11:53:57 -0300

    axios

--- tp_final/screens/horaytemperatura.js
+++ tp_final/screens/horaytemperatura.js
@@ -1,5 +1,5 @@
 /* La aplicación debe contar con una pantalla de visualización de la fecha y hora, temperatura actual (API de clima / Axios) y ubicación.
- La Temperatura se debe poder obtener según el geoposicionamiento del dispositivo (Location). 
+ La Temperatura se debe poder obtener según el geoposicionamiento  del dispositivo (Location). 
 */
 import { View } from "react-native";
 
