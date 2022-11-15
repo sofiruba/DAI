ced9714d7351b8bc6a54e1a35ddb272ca4da1e1f
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-11-15 00:27:50 -0300

    llamada api de clima

--- tp_final/screens/horaytemperatura.js
+++ tp_final/screens/horaytemperatura.js
@@ -1,41 +1,33 @@
 /* La aplicación debe contar con una pantalla de visualización de la fecha y hora, temperatura actual (API de clima / Axios) y ubicación.
  La Temperatura se debe poder obtener según el geoposicionamiento  del dispositivo (Location). 
 */
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import axios from 'axios'
+import {useState, useEffect} from 'react'
 export default function Clima(){
     const [temp, setTemp] = useState({temp: 0, temp_min: 0, temp_max: 0})
-    const Clima = useContext(ClimaContext)
-    let ciudad = Clima.ciudad.charAt(0).toUpperCase() + Clima.ciudad.slice(1);
-    const q =  `${ciudad},${Clima.pais}`
+    const q =  `buenos aires,AR`
     
     const getClima = () => {
         return axios.get('http://api.openweathermap.org/data/2.5/weather', {params:{q : q, APPID: '467eb2e2a1738c82e813a30610d7c354' }})
         .then(res => {
             if(res.status === 200){
                 setTemp(res.data.main)
-                cont.innerText= ''
             }
 
         })
         .catch(err => {
-
-            if (err.response.status === 404 ){
-                cont.innerText = 'Vuelve a ingresar correctamente'
-                cont.style.color = 'red'
-            }
-            if (err.response.status === 400 ){
-                cont.innerText = 'Debes ingresar'
-                cont.style.color = 'red'
-            }
+            console.log(err)
         }
-            )
+        )
     }
 
     useEffect(()=> {
         getClima()
     })
     return(
-        <View></View>
+        <View>
+            <Text>Temperatura actual: {parseFloat(temp.temp- 273.15).toFixed(2)}° </Text>
+        </View>
     )
 }
