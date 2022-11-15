403397c6620983a5b3cbd001d6888965298a4b88
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-11-15 01:20:49 -0300

    casi terminado

--- tp_final/screens/horaytemperatura.js
+++ tp_final/screens/horaytemperatura.js
@@ -1,15 +1,55 @@
 /* La aplicación debe contar con una pantalla de visualización de la fecha y hora, temperatura actual (API de clima / Axios) y ubicación.
  La Temperatura se debe poder obtener según el geoposicionamiento  del dispositivo (Location). 
 */
-import { View, Text } from "react-native";
+import { View, Text, PermissionsAndroid} from "react-native";
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import GeoLocation from 'react-native-geolocation-service'
 export default function Clima(){
     const [temp, setTemp] = useState({temp: 0, temp_min: 0, temp_max: 0})
+    const [location, setLocation] = useState(null)
     const q =  `buenos aires,AR`
+
+    const permisoDeUbicacion = async () => {
     
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: "Permiso de ubicacion",
+                    message: "La app necesita acceder a tu ubicacion",
+                    buttonNeutral: "Mas tarde",
+                    buttonNegative: "Cancelar",
+                    buttonPositive: "OK"
+                },
+            )
+            console.log(granted)
+            if(granted === PermissionsAndroid.RESULTS.GRANTED){
+                return true
+            
+            }
+            else {
+                return false
+            }
+        
+
+    }
+
+    const obtenerUbicacion = async () => {
+        const resultado = await permisoDeUbicacion()
+        if(resultado){
+            GeoLocation.getCurrentPosition(
+                position => {
+                    setLocation(position)
+                },
+                error => {
+                    setLocation(null)
+                },
+                {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+            )
+        }
+    }
     const getClima = () => {
-        return axios.get('http://api.openweathermap.org/data/2.5/weather', {params:{q : q, APPID: '467eb2e2a1738c82e813a30610d7c354' }})
+        return axios.get('http://api.openweathermap.org/data/2.5/weather', {params:{lat : location.latitude, lon: location.longitude, APPID: '467eb2e2a1738c82e813a30610d7c354' }})
         .then(res => {
             if(res.status === 200){
                 setTemp(res.data.main)
@@ -27,6 +67,7 @@ export default function Clima(){
     })
     return(
         <View>
+            <Text>lat : {location.latitude } long: {location.longitude}</Text>
             <Text>Temperatura actual: {parseFloat(temp.temp- 273.15).toFixed(2)}° </Text>
         </View>
     )
