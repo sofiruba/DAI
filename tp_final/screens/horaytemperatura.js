d83ab4352012f4fa4a4dc7d79c257e70db73885e
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-11-16 22:14:48 -0300

    A ver si este funciona

--- tp_final/screens/horaytemperatura.js
+++ tp_final/screens/horaytemperatura.js
@@ -1,79 +1,78 @@
-/* La aplicación debe contar con una pantalla de visualización de la fecha y hora, temperatura actual (API de clima / Axios) y ubicación.
- La Temperatura se debe poder obtener según el geoposicionamiento  del dispositivo (Location). 
-*/
-import { View, Text, PermissionsAndroid} from "react-native";
-import axios from 'axios'
-import {useState, useEffect} from 'react'
-import GeoLocation from 'react-native-geolocation-service'
-import { Button } from "react-native-web";
-export default function Clima(){
-    const [temp, setTemp] = useState({temp: 0, temp_min: 0, temp_max: 0})
-    const [location, setLocation] = useState(null)
-    const q =  `buenos aires,AR`
-
-    const permisoDeUbicacion = async () => {
-    
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                {
-                    title: "Permiso de ubicacion",
-                    message: "La app necesita acceder a tu ubicacion",
-                    buttonNeutral: "Mas tarde",
-                    buttonNegative: "Cancelar",
-                    buttonPositive: "OK"
-                },
-            )
-            console.log(granted)
-            if(granted === PermissionsAndroid.RESULTS.GRANTED){
-                return true
-            
-            }
-            else {
-                return false
-            }
-        
-
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, Button, PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+// Function to get permission for location
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
     }
-
-    const obtenerUbicacion = async () => {
-        const resultado = await permisoDeUbicacion()
-        if(resultado){
-            GeoLocation.getCurrentPosition(
-                position => {
-                    setLocation(position)
-                },
-                error => {
-                    setLocation(null)
-                },
-                {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
-            )
-        }
-    }
-    const getClima = () => {
-        obtenerUbicacion()
-        return axios.get('http://api.openweathermap.org/data/2.5/weather', {params:{lat : location.coords.latitude, lon: location.coords.longitude, APPID: '467eb2e2a1738c82e813a30610d7c354' }})
-        .then(res => {
-            if(res.status === 200){
-                setTemp(res.data.main)
-            }
-
-        })
-        .catch(err => {
-            console.log(err)
-        }
-        )
-    }
-
-    useEffect(()=> {
-        obtenerUbicacion()
-    })
-
-    console.log(location)
-    return(
-        <View>
-            <Text>lat: {location.coords.latitude } long: {location.coords.longitude}</Text>
-            <Text>Temperatura actual: {parseFloat(temp.temp- 273.15).toFixed(2)}° </Text>
-            <Button title='Obtener clima' onClick={getClima} ></Button>
-        </View>
-    )
-}
+  } catch (err) {
+    return false;
+  }
+};
+const Clima = () => {
+  // state to hold location
+  const [location, setLocation] = useState(false);
+  // function to check permissions and get Location
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Get Location" onPress={getLocation} />
+      </View>
+      <Text>Latitude: {location ? location.coords.latitude : null}</Text>
+      <Text>Longitude: {location ? location.coords.longitude : null}</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Send Location" />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default Clima;
