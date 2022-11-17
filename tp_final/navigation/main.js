7caaf4593f92ac36e4f73292abf133af450740eb
Delfi <delfinamunich@gmail.com>
2022-11-17 14:01:49 -0300

    context

--- tp_final/navigation/main.js
+++ tp_final/navigation/main.js
@@ -5,17 +5,23 @@ import Home from '../screens/home';
 import Contactos from '../screens/contactos';
 import Clima from '../screens/horaytemperatura';
 import CambioImagen from '../screens/cambioimagen';
-const Stack = createNativeStackNavigator();
+import { FondoProvider } from '../context/fondocontext';
+import { useState } from 'react';
+
 export default function Main() {
+  const Stack = createNativeStackNavigator();
+  const [fondo, setFondo] = React.useState('');
+
   return (
-    <NavigationContainer  >
+    <FondoProvider value={{ fondo, setFondo }}>
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Contactos" component={Contactos} options={{ headerShown: false }} />
         <Stack.Screen name="Clima" component={Clima} options={{ headerShown: false }} />
         <Stack.Screen name="CambioImagen" component={CambioImagen} options={{ headerShown: false }} />
-
       </Stack.Navigator>
     </NavigationContainer>
+    </FondoProvider>
   )
 }
